33e7afe8eeb73a0fe53b4e41e11b2e9bebb08b6b
Thamyris Lopes <101022477+thamyrislopes@users.noreply.github.com>
2023-02-21 21:38:35 -0300

    update

---
+++ exercicios/ex013/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <=6)
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+} Teste lógico no início*/
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} Teste lógico no final*/
